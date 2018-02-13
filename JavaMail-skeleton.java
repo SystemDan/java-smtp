50fe5ac9d8dea09d7abb4ac700e55368f4a43d4d
Danny Lee <Wallou@users.noreply.github.com>
2018-02-13 10:38:16 -0500

    Added skeleton code for JavaMail

---
+++ JavaMail-skeleton.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+import java.util.Properties; 
+import java.util.Scanner;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Authenticator;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class JavaMail {
+
+    final static String SYSTEM_EMAIL = “”;
+    final static String SYSTEM_NAME = "";
+    final static String PASSWORD = “”;
+    
+    public static void main(String[] args) {
+
+    }
+
+    public static void sendMail(String email, String subject, String body) {
+
+    }
+
+    public static Properties initProperty() {
+
+    }
+
+    public static Authenticator initAuth() {
+
+    }
+}
