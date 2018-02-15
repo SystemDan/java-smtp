3d00c1001ef3610f0f3fe312b4715eb64307de05
Danny Lee <Wallou@users.noreply.github.com>
2018-02-15 15:54:40 -0500

    Removed unused imports

--- JavaMail.java
+++ JavaMail.java
@@ -1,27 +1,19 @@
 import java.util.Date;
 import java.util.Properties; 
 import java.util.Scanner;
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
-import javax.mail.BodyPart;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 public class JavaMail {
 
     final static String SYSTEM_EMAIL = "csci4050@gmail.com";
     final static String SYSTEM_NAME = "CSCI 4050";
-    final static String PASSWORD = “some password”;
+    final static String PASSWORD = "some password";
     
     public static void main(String[] args) {
 	String email, subject, body;
