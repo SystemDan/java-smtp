2e3e7eb2d3e552ac190a879a723f341816f373cd
Danny Lee <danny@wallou.net>
2018-02-20 14:46:48 -0500

    Added tutorial on SimpleJavaMail + cleanup on libraries

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Basic mailing system in Java for CSCI 4050
 
 Please note that this implementation requires that the system's Gmail account be in insecure mode. 
 See: https://myaccount.google.com/lesssecureapps 
+
+This implementation uses both JavaMail and SimpleJavaMail.
---
+++ skeletons/SimpleJavaMail-skeleton.java
@@ -0,0 +1,19 @@
+import org.simplejavamail.email.Email;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+import org.simplejavamail.mailer.config.TransportStrategy;
+
+public class SimpleJavaMail {
+
+    final static String SYSTEM_EMAIL = "csci4050@gmail.com";
+    final static String SYSTEM_NAME = "CSCI 4050";
+    final static String PASSWORD = "some password";
+    
+    public static void main(String[] args) {
+
+    }
+    
+    public static void sendMail(String email, String subject, String body) {
+
+    }
+}
